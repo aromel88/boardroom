5f10d54119d0117fd91bec1d2d4eb7767e8bdf27
aromel88 <aromel8838@gmail.com>
2017-08-22 13:04:23 -0400

    Remove old comment

--- client/src/js/components/AppContainer.js
+++ client/src/js/components/AppContainer.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ChatContainer from './chat/ChatContainer';
 import CanvasContainer from './canvas/CanvasContainer';
 
-// test class to make sure react is working
 class AppContainer extends React.Component {
 
   constructor() {
